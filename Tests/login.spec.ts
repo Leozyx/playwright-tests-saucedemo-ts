c5bdca8939ca13bceddbd06f3a9d6b21576757b1
Leozyx <leonardoviniciusmagalhaes@gmail.com>
2025-07-23 20:01:38 -0300

    feat: usar variáveis de ambiente para login

--- Tests/login.spec.ts
+++ Tests/login.spec.ts
@@ -2,18 +2,21 @@ import { test } from '@playwright/test'
 import { LoginPage } from '../Pages/LoginPage'
 import { HomePage } from '../Pages/HomePage'
 
+const user = process.env.LOGIN_USER || ''
+const password = process.env.LOGIN_PASSWORD || ''
+
 test('Falid login of page - password', async ({ page }) => {
   const loginPage = new LoginPage(page)
 
   await loginPage.goto()
-  await loginPage.fillLogin('standard_user', 'anything')
+  await loginPage.fillLogin(user, 'anything')
 })
 
 test('Falid login of page - user', async ({ page }) => {
   const loginPage = new LoginPage(page)
 
   await loginPage.goto()
-  await loginPage.fillLogin('anything', 'secret_sauce')
+  await loginPage.fillLogin('anything', password)
 })
 
 test('Corect login and verification of page', async ({ page }) => {
@@ -21,6 +24,7 @@ test('Corect login and verification of page', async ({ page }) => {
   const homePage = new HomePage(page)
 
   await loginPage.goto()
-  await loginPage.fillLogin('standard_user', 'secret_sauce')
+  await loginPage.fillLogin(user, password)
+  await homePage.checkPage()
   await homePage.checkPage()
 })
