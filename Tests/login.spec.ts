602fbeaafe7a1387de49c178b2615ce17f684d39
Leozyx <leonardoviniciusmagalhaes@gmail.com>
2025-07-23 21:41:53 -0300

    Test

--- Tests/login.spec.ts
+++ Tests/login.spec.ts
@@ -2,29 +2,28 @@ import { test } from '@playwright/test'
 import { LoginPage } from '../Pages/LoginPage'
 import { HomePage } from '../Pages/HomePage'
 
-const user = process.env.LOGIN_USER || ''
-const password = process.env.LOGIN_PASSWORD || ''
-
 test('Falid login of page - password', async ({ page }) => {
   const loginPage = new LoginPage(page)
 
   await loginPage.goto()
-  await loginPage.fillLogin(user, 'anything')
+  await loginPage.fillLogin('standard_user', 'anything')
 })
 
 test('Falid login of page - user', async ({ page }) => {
   const loginPage = new LoginPage(page)
 
   await loginPage.goto()
-  await loginPage.fillLogin('anything', password)
+  await loginPage.fillLogin('anything', 'secret_sauce')
 })
 
 test('Corect login and verification of page', async ({ page }) => {
   const loginPage = new LoginPage(page)
   const homePage = new HomePage(page)
 
+  const user = process.env.LOGIN_USER!
+  const password = process.env.LOGIN_PASSWORD!
+
   await loginPage.goto()
   await loginPage.fillLogin(user, password)
-  await homePage.checkPage()
   await homePage.checkPage()
 })
